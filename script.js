1e7d53a5a3c35742dcac1d61ec1e26f8f7b2b82a
priyanxxu <priyanshu09u@gmail.com>
2026-04-06 16:58:17 +0530

    cse 26

---
+++ script.js
@@ -0,0 +1,25 @@
+//console.log("hello world");
+//document.write("hello world");
+//var num = 10;
+//console.log(num);
+//console.log(typeof(num));
+//num=false;
+//console.log(num);
+//console.log(typeof(num));
+//const sym1= Symbol(4);
+//console.log(sym1);
+//const sym2 = Symbol(4);
+//console.log(sym2);
+//if(sym1==sym2){
+//    console.log("true")
+//}
+//else{
+//    console.log("false");
+//}
+//var a=100; var b=13; var c= 10; var linebreak=<br>
+//document.write("a+b+c=");
+//result= a+b+c;
+let number = [10,20,30];
+let sum = number.reduce((total,num) => total +num,0);
+//let result = number.filter ( num => num > 15);
+console.log(sum);
